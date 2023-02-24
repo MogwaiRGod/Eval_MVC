9116a76423b73ec54c5af640b98081abab73c610
Diane <diane.roberg@gmail.com>
2023-02-24 23:53:03 +0100

    organisation des fichiers vues

--- view/scripts/main.js
+++ view/scripts/main.js
@@ -1,14 +1,18 @@
+// chemin à la base de notre API : VERIFIER QUE LE PORT EST LE BON
+const apiBaseUrl = "http://localhost:2000/";
+
 /*
  * Fichier .js qui contient toutes les fonctions nécessaires au traitement des requêtes envoyées côté client
  * Utilise AJAX (jQuery)
  */
 
-/*
+/**
  * FONCTIONS GENERALES
+ * 
+ * fonction qui ajoute une rangée à un tableau parent (parent) entré en argument avec un nombre
+ * de cellules entré en argument (nbCells), avec le contenu de la librairie (lib)
+ * ne retourne rien car ajoute directement la rangée au tableau HTML
  */
-// fonction qui ajoute une rangée à un tableau parent (parent) entré en argument avec un nombre
-// de cellules entré en argument (nbCells), avec le contenu de la librairie (lib)
-// ne retourne rien car ajoute directement la rangée au tableau HTML
 function addRow(parent, nbCells, lib) {
 
     // on boucle dans le tableau
@@ -29,43 +33,120 @@ function addRow(parent, nbCells, lib) {
 }
 
 
+/**
+ * FONCTIONS SPECIFIQUES
+ * 
+ * pour manage_albums.html
+ * 
+ * fonction qui reset une mention d'erreur dans les inputs
+ */
+function resetAside() {
+    if (document.getElementById("tmp-aside")) {
+        document.getElementById("tmp-aside").setAttribute('id', 'default-aside');
+    }
+    document.getElementById("default-aside").textContent = "*Champs obligatoires";
+    return;
+}
+
+function resetManageAlbums() {
+    document.getElementById("label-menu-membership").style.display = 'none';
+    document.getElementById("menu-membership").style.display = 'none';
+    document.getElementById("label-menu-purchases").style.display = 'none';
+    document.getElementById("menu-purchases").style.display = 'none';
+    document.getElementById("menu-membership").style.display = 'none';
+    document.getElementById("label-menu-action").style.display = 'none';
+    document.getElementById("menu-action").style.display = 'none';
+    return;
+}
+
+// fonction qui affiche un menu selon le service sélectionné
+function getMenuSrvc(srvc) {
+    switch (srvc) {
+        case 'membership':
+        case 'purchases':
+            setMenu(srvc);
+            setMenuPlatform (srvc);
+            return;
+        case 'local':
+            // Pas de plateforme si on a choisi un import local
+            optionPlatform = "";    
+            // On peut directement ouvrir le menu d'options
+            setMenu('action');
+            return;
+    } // FIN SWITCH SRVC
+} // FIN GET MENU
+
+// fonction qui affiche un menu selon une catégorie entrée en argument
+function setMenu(type) {
+    document.getElementById(`label-menu-${type}`).style.display = 'block';
+    document.getElementById(`menu-${type}`).style.display = 'block';
+    return;
+}
+
+// fonction qui affiche le formulaire selon l'action choisie par le client
+function setForm(opt) {
+    switch (opt) {
+        case 'add':
+            document.getElementById("action").textContent = "Ajouter";
+            break;
+        case 'delete':
+            document.getElementById("action").textContent = "Supprimer";
+            break;
+        case 'update':
+            document.getElementById("action").textContent = "Mettre à jour";
+            break;
+    }
+    document.getElementById("last-form-albums").style.display = 'flex';
+    document.getElementById("btn-form").style.display = 'block';    
+    return;
+} // FIN SET FORM
+
+function setMenuPlatform (srvc) {
+    // une fois que l'utilisateur a fait son choix de plateforme
+    document.getElementById(`menu-${srvc}`).addEventListener('change', function () {
+        console.log('clic')
+        // stockage de la plateforme
+        optionPlatform = $(`#menu-${srvc}`).val();
+
+        // affichage de la bibliotèque triée par plateforme
+        showLibraryMini(srvc, $( "#lib-albums" ), optionPlatform);
+        
+        // on ouvre le menu d'action
+        setMenu('action');
+        
+        // au choix d'une action dans le menu action
+        $("#menu-action").on("change", function() {
+            // stockage de l'action
+            optionAction = $("#menu-action").val();
+            // affichage du formulaire
+            setForm(optionAction);
+        });
+    });
+    return;
+}
+
+
+
+/**
+ * FONCTIONS DU CRUD
+ */
+
+
+
+/**
+ * CREATE
+ */
+
+
+/* PLUS LE TEMPS */
+// fonction permettant de demander à ajouter un album à la BDD
+function addAlbum (service, title, artist, platform, id) {
+
+}
 
 /*
- * TRAITEMENT DES REQUÊTES
- * fonctions et event listeners
- */
-// fonction qui se lance dès que le document de base (ici : index.html) est entièrement chargé <=> ready
-$(document).ready(() => {
-    /*
-     * VARIABLES ET CONSTANTES
-     */
-    // chemin à la base de notre API VERIFIER QUE LE PORT EST LE BON
-    const apiBaseUrl = "http://localhost:2000/";
-    // les conteneurs de biliothèque
-    const allContainers = document.getElementsByClassName("containers-lib");
-
-    // affiche toute la bibliothèque à l'ouverture de la page correspondante
-    showLibrary();
-
-
-
-    /*
-     * FONCTIONS DU CRUD
-     */
-
-    /*
-     * CREATE
-     */
-
-    /* PLUS LE TEMPS */
-    // fonction permettant de demander à ajouter un album à la BDD
-    function addAlbum (service, title, artist, platform, id) {
-
-    }
-
-    /*
-     * READ
-     */
+* READ
+*/
 
     // pour afficher la bibliothèque complète sur le site
     function showLibrary() {
@@ -140,6 +221,8 @@ $(document).ready(() => {
             type: "GET",
             /* route pour lire la bibliothèque en entier */
             url: apiBaseUrl + "library/" + filter,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
 
             /* 
                 * result correspond à ce que la requête a renvoyé si elle a été un succès
@@ -147,7 +230,8 @@ $(document).ready(() => {
             success: (result) => {
                 /* result est l'objet JSON contenant la bibliohèque complète sous forme d'objet JSON */
                 // si la requête est un succès
-                $(`container-${filter}`).style.display = 'block';
+                console.log(document.getElementById(`container-${filter}`).style.display)
+                document.getElementById(`container-${filter}`).style.display = 'block';
             },
             error: (xhr, status, error) => {
                 // sinon
@@ -196,14 +280,14 @@ $(document).ready(() => {
         let route;
         (!platform) ? route = apiBaseUrl + "library/" + service : route = apiBaseUrl + "library/" + service + "/" + platform;
         
-        console.log(route);
 
         $.ajax(
             {
 
             type: "GET",
-
             url: route,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
 
             success: (result) => {
                 /* result est l'objet JSON contenant la bibliohèque complète sous forme d'objet JSON */
@@ -263,7 +347,7 @@ $(document).ready(() => {
                 document.getElementById("tmp-aside").textContent = JSON.stringify(result.message);
             },
             error: (xhr, status, error) => {
-                document.getElementById("tmp-aside").textContent = error + "Une erreur s'est produite";
+                document.getElementById("tmp-aside").textContent = "L'album demandé n'existe pas";
             }
         });
     }
@@ -279,251 +363,69 @@ $(document).ready(() => {
         $.ajax(
             {
             type: "DELETE",
-
             url: apiBaseUrl + "library/" + service + "/" + id,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
 
             success: (result) => {
                 // on rappelle la fonction d'affichage pour voir les données mises à jour
                 showLibraryMini(service, container, platform ="");
 
                 // message de suppression
-                setTimeout(() => {  document.getElementById("tmp-aside").textContent = "Album supprimé avec succès !"; }, 5000);
+                document.getElementById("tmp-aside").textContent = JSON.stringify(result.message);
             },
             error: (xhr, status, error) => {
-                document.getElementById("tmp-aside").textContent = error + "Une erreur s'est produite"
+                document.getElementById("tmp-aside").textContent = "L'album demandé n'existe pas";
             }
         });
     } // FIN DELETE ALBUM
 
-
+/*
+ * TRAITEMENT DES REQUÊTES
+ * fonctions et event listeners
+ */
+// fonction qui se lance dès que le document de base est entièrement chargé <=> ready
+$(document).ready(() => {
     /*
+     * VARIABLES ET CONSTANTES
+     */
+    
+    // les conteneurs de biliothèque
+    const allContainers = document.getElementsByClassName("containers-lib");
+    // détermine quelle page en chargée
+    var loadedPage = location.href.split("/").slice(-1);
+
+
+   
+
+
+    /**
+     * 
      * EVENT LISTENERS
+     * 
      */
 
-    /* 
+    /**
      * Event listeners des filtres de la bibliothèque
-     */  
-    // on sélectionne les filtres
-    document.querySelectorAll(".filter").forEach(e => {
-        // au clic d'un filter
-        e.addEventListener('click', () => {
-            // on récupère le filtre
-            const filter = e.id.substring(2);
-            if (filter === "all") {
-                showLibrary();
-                return;
-            }
-            // on lance la fonction pour afficher la bibliothèque filtrée
-            showLibraryFilter(filter);
-        });
-    });
+     */ 
+    if (loadedPage[0] === 'library.html') {
+        // affiche toute la bibliothèque à l'ouverture de la page
+        showLibrary();
 
-
-    /* 
-     * Event listeners du formulaire de la page de gestion des albums
-     */
-
-    // J'ai plus le temps, le code est fait à l'arrache déso ! -> code à 0% optimisé :S
-
-    /* Pour le formulaire : les informations obligatoires à entrer varient selon l'action choisie -> pas le
-     * temps de le faire. 
-     * A chaque choix de menu déroulant, la bibliothèque correspondante devra s'afficher
-     * */
-
-    const dropdownService = document.getElementById("menu-service");
-    let optionService; let optionPlatform; let optionAction;
-
-    // lorsqu'une option de SERVICE est sélectionnée
-    dropdownService.addEventListener('change', (event) => {
-        /* RESET DE LA PAGE */
-        // on reset la mention du formulaire qui apparaîtra plus tard
-        if (document.getElementById("tmp-aside")) {
-            document.getElementById("tmp-aside").textContent = "*Champs obligatoires";
-            document.getElementById("tmp-aside").setAttribute('id', 'default-aside');
-        }
-
-        // Reset
-        document.getElementById("label-menu-membership").style.display = 'none';
-        document.getElementById("menu-membership").style.display = 'none';
-        document.getElementById("label-menu-purchases").style.display = 'none';
-        document.getElementById("menu-purchases").style.display = 'none';
-        document.getElementById("label-menu-action").style.display = 'none';
-        document.getElementById("menu-action").style.display = 'none';
-
-        // stockage de l'option sélectionnée par le client
-        optionService = $("#menu-service").val();
-
-        // On va afficher la bibliothèque du service correspondant
-        showLibraryMini(optionService, $( "#lib-albums" ));
-        
-        // Selon l'option sélectionnée, on va afficher le prochain menu 
-        switch (optionService) {
-            case 'membership':
-                document.getElementById("label-menu-membership").style.display = 'block';
-                document.getElementById("menu-membership").style.display = 'block';
-                break;
-            case 'purchases':
-                document.getElementById("label-menu-purchases").style.display = 'block';
-                document.getElementById("menu-purchases").style.display = 'block';
-                break;
-            case 'local':
-                // Pas de plateforme si on a choisi un import local
-                optionPlatform = "";
-                document.getElementById("label-menu-action").style.display = 'block';
-                document.getElementById("menu-action").style.display = 'block';
-
-                // On peut directement ouvrir le menu d'options
-                document.getElementById("menu-action").addEventListener('change', () => {
-                    if (document.getElementById("tmp-aside")) {
-                        document.getElementById("tmp-aside").textContent = "*Champs obligatoires";
-                        document.getElementById("tmp-aside").setAttribute('id', 'default-aside');
-                    }
-
-                    optionAction = $("#menu-action").val();
-
-                    switch (optionAction) {
-                        case 'add':
-                            document.getElementById("action").textContent = "Ajouter";
-                            break;
-                        case 'delete':
-                            document.getElementById("action").textContent = "Supprimer";
-                            break;
-                        case 'update':
-                            document.getElementById("action").textContent = "Mettre à jour";
-                            break;
-                    }
-
-                    document.getElementById("last-form-albums").style.display = 'flex';
-                    document.getElementById("btn-form").style.display = 'block';
-                });
-                break;
-        }
-    });
-
-    // déploiement du formulaire si on a choisi le service abonnement => CHOIX DE LA PLATEFORME
-    document.getElementById("menu-membership").addEventListener('change', (event) => {
-        if (document.getElementById("tmp-aside")) {
-            document.getElementById("tmp-aside").textContent = "*Champs obligatoires";
-            document.getElementById("tmp-aside").setAttribute('id', 'default-aside');
-        }
-
-        // stockage de la plateforme
-        optionPlatform = $("#menu-membership").val();
-
-        // affichage de la bibliotèque triée par plateforme
-        showLibraryMini(optionService, $( "#lib-albums" ), optionPlatform);
-
-        document.getElementById("label-menu-action").style.display = 'block';
-        document.getElementById("menu-action").style.display = 'block';
-
-        document.getElementById("menu-action").addEventListener('change', () => {
-            if (document.getElementById("tmp-aside")) {
-                document.getElementById("tmp-aside").textContent = "*Champs obligatoires";
-                document.getElementById("tmp-aside").setAttribute('id', 'default-aside');
-            }
-
-            // stockage de l'action
-            optionAction = $("#menu-action").val();
-
-            switch (optionAction) {
-                case 'add':
-                    document.getElementById("action").textContent = "Ajouter";
-                    break;
-                case 'delete':
-                    document.getElementById("action").textContent = "Supprimer";
-                    break;
-                case 'update':
-                    document.getElementById("action").textContent = "Mettre à jour";
-                    break;
-            }
-
-            document.getElementById("last-form-albums").style.display = 'flex';
-            document.getElementById("btn-form").style.display = 'block';
-        });
-    });
-
-    // déploiement du formulaire si on a choisi le service achat
-    document.getElementById("menu-purchases").addEventListener('change', (event) => {
-        if (document.getElementById("tmp-aside")) {
-            document.getElementById("tmp-aside").textContent = "*Champs obligatoires";
-            document.getElementById("tmp-aside").setAttribute('id', 'default-aside');
-        }
-
-        // stockage de la plateforme
-        optionPlatform = $("#menu-purchases").val();
-
-        // affichage de la bibliotèque triée par plateforme
-        showLibraryMini(optionService, $( "#lib-albums" ), optionPlatform);
-
-        document.getElementById("label-menu-action").style.display = 'block';
-        document.getElementById("menu-action").style.display = 'block';
-
-        document.getElementById("menu-action").addEventListener('change', () => {
-            // stockage de l'action choisie
-            optionAction = $("#menu-action").val();
-
-            switch (optionAction) {
-                case 'add':
-                    document.getElementById("action").textContent = "Ajouter";
-                    break;
-                case 'delete':
-                    document.getElementById("action").textContent = "Supprimer";
-                    break;
-                case 'update':
-                    document.getElementById("action").textContent = "Mettre à jour";
-                    break;
-            }
-
-            document.getElementById("last-form-albums").style.display = 'flex';
-            document.getElementById("btn-form").style.display = 'block';
-        });
-    });
-
-    // avant l'envoi du formulaire (= au clic du bouton Valider) on va récupérer les inputs
-    // et vérifier leur intégrité
-    document.getElementById("btn-form").addEventListener('click', () => {
-        const optionId = $("#ipt-id").val();
-        const optionName = $("#ipt-name").val();
-        const optionArtist = $("#ipt-artist").val();
-
-        console.log(optionId);
-
-        if (document.getElementById("default-aside")) {
-            document.getElementById("default-aside").setAttribute('id', 'tmp-aside');
-        }
-
-        switch (optionAction) {
-            case 'add':
-                // si les inputs sont incomplets
-                if (!optionName || !optionArtist) {
-                    document.getElementById("tmp-aside").textContent = "Veuillez remplir les champs obligatoires";
-                    break;
-                } else {
-                    // on invoque la fonction d'ajout d'album à la BDD
+        // on sélectionne les filtres
+        document.querySelectorAll(".filter").forEach(e => {
+            // au clic d'un filter
+            e.addEventListener('click', () => {
+                // on récupère le filtre
+                const filter = e.id.substring(2);
+                if (filter === "all") {
+                    showLibrary();
+                    return;
                 }
-                break;
-
-            case 'delete':
-                // si les inputs sont incomplets
-                if (!optionId) {
-                    document.getElementById("tmp-aside").textContent = "Veuillez remplir les champs obligatoires";
-                    break;
-                } else {
-                    // on invoque la fonction de suppression
-                    deleteAlbum ($( "#lib-albums" ), optionService, optionId);
-                }
-                break;
-            case 'update':
-                // si les inputs sont incomplets
-                if (!optionId || !(optionName || optionArtist || optionPlatform)) {
-                    document.getElementById("tmp-aside").textContent = "Veuillez remplir les champs obligatoires";
-                    break;
-                } else {
-                    // on invoque la fonction de màj d'album
-                    updateAlbum ($( "#lib-albums" ), optionService, optionId, [{"name": optionName}, {"artist":optionArtist}, {"platform":optionPlatform}]);
-                }
-        }
-    });
-
+                // on lance la fonction pour afficher la bibliothèque filtrée
+                showLibraryFilter(filter);
+            });
+        });
+    } // FIN LIBRARY. HTML
 
 }); // FIN DOCUMENT.READY()
---
+++ view/scripts/manage_albums.js
@@ -0,0 +1,175 @@
+
+    //les inputs utilisateur pour chaque menu déroulant
+    let optionService; let optionPlatform; let optionAction;
+
+/**
+ * FONCTIONS SPECIFIQUES
+ * à manage_albums.html
+ * 
+ * 
+ * fonction qui reset une mention d'erreur dans les inputs
+ */
+function resetAside() {
+    if (document.getElementById("tmp-aside")) {
+        document.getElementById("tmp-aside").setAttribute('id', 'default-aside');
+    }
+    document.getElementById("default-aside").textContent = "*Champs obligatoires";
+    return;
+}
+
+function resetManageAlbums() {
+    document.getElementById("label-menu-membership").style.display = 'none';
+    document.getElementById("menu-membership").style.display = 'none';
+    document.getElementById("label-menu-purchases").style.display = 'none';
+    document.getElementById("menu-purchases").style.display = 'none';
+    document.getElementById("menu-membership").style.display = 'none';
+    document.getElementById("label-menu-action").style.display = 'none';
+    document.getElementById("menu-action").style.display = 'none';
+    return;
+}
+
+// fonction qui affiche un menu selon le service sélectionné
+function getMenuSrvc(srvc) {
+    switch (srvc) {
+        case 'membership':
+        case 'purchases':
+            setMenu(srvc);
+            setMenuPlatform (srvc);
+            return;
+        case 'local':
+            // Pas de plateforme si on a choisi un import local
+            optionPlatform = "";    
+            // On peut directement ouvrir le menu d'options
+            setMenuAction(); 
+            return;
+    } // FIN SWITCH SRVC
+} // FIN GET MENU
+
+// fonction qui affiche un menu selon une catégorie entrée en argument
+function setMenu(type) {
+    document.getElementById(`label-menu-${type}`).style.display = 'block';
+    document.getElementById(`menu-${type}`).style.display = 'block';
+    return;
+}
+
+// fonction qui affiche le formulaire selon l'action choisie par le client
+function setForm(opt) {
+    switch (opt) {
+        case 'add':
+            document.getElementById("action").textContent = "Ajouter";
+            break;
+        case 'delete':
+            document.getElementById("action").textContent = "Supprimer";
+            break;
+        case 'update':
+            document.getElementById("action").textContent = "Mettre à jour";
+            break;
+    }
+    document.getElementById("last-form-albums").style.display = 'flex';
+    document.getElementById("btn-form").style.display = 'block';    
+    return;
+} // FIN SET FORM
+
+function setMenuPlatform (srvc) {
+    // une fois que l'utilisateur a fait son choix de plateforme
+    document.getElementById(`menu-${srvc}`).addEventListener('change', function () {
+        // stockage de la plateforme
+        optionPlatform = $(`#menu-${srvc}`).val();
+
+        // affichage de la bibliotèque triée par plateforme
+        showLibraryMini(srvc, $( "#lib-albums" ), optionPlatform);
+        
+        setMenuAction();
+    });
+    return;
+}
+
+function setMenuAction() {
+    // on ouvre le menu d'action
+    setMenu('action');
+    
+    // au choix d'une action dans le menu action
+    $("#menu-action").on("change", function() {
+        // stockage de l'action
+        optionAction = $( "#menu-action" );
+        console.log(optionAction.val())
+        // affichage du formulaire
+        setForm(optionAction.val());
+    });
+    return;
+}
+
+
+/**
+ * SCRIPT
+ */
+
+// une fois que la page est chargée
+$(document).ready( () => {
+    // on réinitialise les éléments de la page
+    resetManageAlbums();
+
+    /** 
+     * EVENT LISTENERS
+     * les menus déroulants apparaissent une fois le précédent ayant eu une option sélectionnée
+     */
+
+    // à la seléction d'une option du menu déroulant de services
+    $("#menu-service").on("change", function() {
+        /* RESET DE LA PAGE */
+        resetAside();
+        resetManageAlbums();
+    
+        // stockage de l'option sélectionnée par le client
+        optionService = $( "#menu-service" ).val();
+    
+        // On va afficher la bibliothèque du service correspondant
+        showLibraryMini(optionService, $( "#lib-albums" ));
+            
+        // Selon l'option sélectionnée, on va afficher le prochain menu 
+        getMenuSrvc(optionService);
+    });
+
+    // avant l'envoi du formulaire (= au clic du bouton Valider) on va récupérer les inputs
+    // et vérifier leur intégrité
+    document.getElementById("btn-form").addEventListener('click', () => {
+        console.log(optionAction.val())
+        const optionId = $("#ipt-id").val();
+        const optionName = $("#ipt-name").val();
+        const optionArtist = $("#ipt-artist").val();
+
+        resetAside();
+
+        switch (optionAction.val()) {
+            case 'add':
+                // si les inputs sont incomplets
+                if (!optionName || !optionArtist) {
+                    document.getElementById("tmp-aside").textContent = "Veuillez remplir les champs obligatoires";
+                    break;
+                } else {
+                    // on invoque la fonction d'ajout d'album à la BDD
+                }
+                break;
+
+            case 'delete':
+                // si les inputs sont incomplets
+                if (!optionId) {
+                    document.getElementById("tmp-aside").textContent = "Veuillez remplir les champs obligatoires";
+                    break;
+                } else {
+                    // on invoque la fonction de suppression
+                    deleteAlbum ($( "#lib-albums" ), optionService, optionId);
+                }
+                break;
+            case 'update':
+                // si les inputs sont incomplets
+                if (!optionId || !(optionName || optionArtist || optionPlatform)) {
+                    document.getElementById("tmp-aside").textContent = "Veuillez remplir les champs obligatoires";
+                    break;
+                } else {
+                    // on invoque la fonction de màj d'album
+                    updateAlbum ($( "#lib-albums" ), optionService, optionId, [{"name": optionName}, {"artist":optionArtist}, {"platform":optionPlatform}]);
+                }
+        }
+    });
+}); // FIN MANAGE_ALBUMS.JS
